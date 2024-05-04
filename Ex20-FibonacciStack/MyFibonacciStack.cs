9dff94f085d22445152b6ace255aeb9ace30bc48
Cauã Rufino de Sá <CauaRufinodeSa@Gmail.com>
2024-05-04 00:44:14 -0300

    Exercise 20 upload, using a stack to save N fibonacci numbers and showing them.

---
+++ Ex20-FibonacciStack/MyFibonacciStack.cs
@@ -0,0 +1,38 @@
+﻿namespace Ex20_FibonacciStack
+{
+    internal class MyFibonacciStack
+    {
+        MyFibonacci? head;
+        int size;
+
+        public MyFibonacciStack()
+        {
+            head = null;
+            size = 0;
+        }
+
+        public void Push(MyFibonacci fibonacci)
+        {
+            size++;
+            fibonacci.SetNext(head);
+            this.head = fibonacci;
+        }
+
+        public MyFibonacci Pop()
+        {
+            MyFibonacci aux = head;
+
+            if (!IsEmpty())
+            {
+                size--;
+                head = head.GetNext();
+            }
+
+            return aux;
+        }
+
+        public int GetSize() { return size; }
+
+        public bool IsEmpty() { return head == null; }
+    }
+}
