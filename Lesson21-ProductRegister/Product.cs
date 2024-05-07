61eb24490f4be03299e83cb5b73f2d5c81aa5ebd
Cauã Rufino de Sá <CauaRufinodeSa@Gmail.com>
2024-05-07 16:14:48 -0300

    Lesson 21 upload.

---
+++ Lesson21-ProductRegister/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace Lesson21_ProductRegister
+{
+    internal class Product
+    {
+        public int id { get; set; }
+        public string description { get; set; }
+        public double price { get; set; }
+        public int quantity { get; set; }
+
+        public Product(int id, string description, double price, int quantity)
+        {
+            this.id = id;
+            this.description = description;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public override string? ToString()
+        {
+            return $"{id};{description};{price};{quantity}";
+        }
+    }
+}
---
+++ Lesson21-ProductRegister/Program.cs
@@ -0,0 +1,98 @@
+﻿using Lesson21_ProductRegister;
+
+internal class Program
+{
+    static Product CreateProduct()
+    {
+        Console.WriteLine("\n>>> PRODUCT REGISTER <<<\n");
+
+        Console.Write("Type product id: ");
+        var id = int.Parse(Console.ReadLine());
+
+        Console.Write("Type product description: ");
+        var description = Console.ReadLine();
+
+        Console.Write("Type product price: $");
+        var price = double.Parse(Console.ReadLine());
+
+        Console.Write("Type product quantity: ");
+        var quantity = int.Parse(Console.ReadLine());
+
+        return new Product(id, description, price, quantity);
+    }
+
+    static void ShowStorage(List<Product> storage)
+    {
+        Console.WriteLine("\n>>> PRODUCTS <<<\n");
+        foreach (var product in storage)
+            Console.WriteLine(product);
+        Console.WriteLine();
+    }
+
+    static bool CheckPathing(string path, string file)
+    {
+        bool check = false;
+
+        if (!Directory.Exists(path))
+            Directory.CreateDirectory(path);
+
+        if (!File.Exists(path + file))
+            File.Create(path + file);
+        else
+            check = true;
+
+        return check;
+    }
+
+    static void SaveFile(List<Product> storage, string path, string file)
+    {
+        if (CheckPathing(path, file))
+        {
+
+            StreamWriter writer = new StreamWriter(path + file);
+
+            foreach (var product in storage)
+                writer.WriteLine(product);
+
+            writer.Close();
+        }
+    }
+
+    static List<Product> LoadFile(string path, string file)
+    {
+        List<Product> storage = new();
+
+        if (CheckPathing(path, file))
+        {
+            string[] data;
+
+            foreach (var line in File.ReadAllLines(path + file))
+            {
+                data = line.Split(";");
+                storage.Add(new Product(int.Parse(data[0]), data[1], double.Parse(data[2]), int.Parse(data[3])));
+            }
+        }
+
+        return storage;
+    }
+
+    private static void Main(string[] args)
+    {
+        string path = @"C:\Data\Week-04\Lesson21\";
+        string file = "storage.txt";
+
+        List<Product> productStorage = new();
+
+        productStorage.Add(CreateProduct());
+        productStorage.Add(CreateProduct());
+
+        ShowStorage(productStorage);
+
+        SaveFile(productStorage, path, file);
+
+        foreach (Product p in LoadFile(path, file))
+            Console.WriteLine(p);
+
+        Console.ReadKey();
+    }
+}
