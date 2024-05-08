2e63639b5b26c6504f464094b48661096d193b74
Cauã Rufino de Sá <CauaRufinodeSa@Gmail.com>
2024-05-08 09:51:07 -0300

    Lesson 22 Try catch upload.

---
+++ Lesson22-TryCatch/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int value = 0;
+
+        do
+        {
+            Console.Write("Type an integer value: ");
+            try
+            {
+                value = int.Parse(Console.ReadLine());
+                break;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("This isn't a number.");
+                Console.WriteLine($"\nException: {e}\n");
+            }
+
+        } while (true);
+
+        Console.WriteLine(value);
+    }
+}
