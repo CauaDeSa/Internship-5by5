34e0862abea9617f59634239caba0652f1a2b685
Cauã Rufino de Sá <CauaRufinodeSa@Gmail.com>
2024-05-22 13:56:16 -0300

    Week 05, 06 late upload.

---
+++ Lesson24-Lambda/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Lesson24_Lambda
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ Lesson24-Lambda/Program.cs
@@ -0,0 +1,18 @@
+﻿using Lesson24_Lambda;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Person> personList = new()
+        {
+            new Person(1, "Caua"),
+            new Person(2, "Pedro")
+        };
+
+        if (personList.Exists((p => p.Name == "Caua")))
+            Console.WriteLine("Found!");
+        else
+            Console.WriteLine("Not found!");
+    }
+}
