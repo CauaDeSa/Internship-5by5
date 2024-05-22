b0d7476c35ae9f61e8a7b20796b14eced3b2d1c6
Cauã Rufino de Sá <CauaRufinodeSa@Gmail.com>
2024-05-22 13:56:16 -0300

    Week 05, 06 late upload.

---
+++ Lesson23-FileStreamTryCatch/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string path = @"C:\Data\Week-04\Lesson23\test.txt";
+        string line; 
+
+        try
+        {
+            StreamWriter writer = new(path);
+            writer.WriteLine("Hello World");
+            writer.WriteLine("-----------");
+            writer.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+
+        try
+        {
+            StreamReader reader = new(path);
+            line = reader.ReadLine();
+
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = reader.ReadLine();
+            }
+
+            reader.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
